04c0c3f578969801f309cb19da46fa6596cf4e93
Lennie Wennerlund <lennie@lempa.se>
2017-10-16 12:04:20 +0200

    work

---
+++ SoftwareClass/Software.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cs.Software
+{
+    public class Software
+    {
+        public void Run()
+        {
+
+            Console.WriteLine("Class running");
+        }
+    }
+}
---
+++ StarterConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StarterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Software starting");
+
+            var sStart = new Cs.Software.Software();
+            sStart.Run();
+
+
+            Console.WriteLine("End!!");
+            Console.ReadLine();
+
+        }
+    }
+}
